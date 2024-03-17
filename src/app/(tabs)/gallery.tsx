c44d87f42f179fb8e3826805105766a4e79a6337
“alansuhe” <tosuhe@gmail.com>
2024-03-17 23:11:40 +0800

    15-16

--- src/app/(tabs)/gallery.tsx
+++ src/app/(tabs)/gallery.tsx
@@ -1,7 +1,13 @@
 import {
   ActivityIndicator,
-  Button, Image, ImageBackground, Modal,
-  Pressable, StyleSheet, Text, View
+  FlatList,
+  Image,
+  Pressable,
+  RefreshControl,
+  ScrollView,
+  Text,
+  View,
+  useWindowDimensions
 } from 'react-native';
 
 // import { Text, View } from '@/components/Themed';
@@ -10,27 +16,46 @@ import { FlashList } from "@shopify/flash-list";
 import { useEffect, useState } from 'react';
 import { getBingWallPapers } from '@/api';
 import { Tabs } from 'expo-router';
-import { Feather, FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import { sizes, useStyle } from '@/style';
 const { l, s, m } = sizes
 
 import WallpaperModal from '@/components/WallpaperModal';
+// import { useSettings } from '@/store';
 
 export default function TabTwoScreen() {
 
+  // const { isDark } = useSettings()
+
   const { colors, text, layout } = useStyle()
+  const { width: screenWidth } = useWindowDimensions()
 
   const [wallpapers, setWallpapers] = useState([])
   const [selectedWallpaperIndex, setSelectedWallpaperIndex] = useState(0)
   const [loading, setLoading] = useState(false)
   const [modalVisible, setModalVisible] = useState(false)
+  const [galleryLayout, setGalleryLayout] = useState('list')
 
-  async function handleGetWallpapers(n: number) {
+  async function handleGetWallpapers(idx: number, n: number) {
     setLoading(true)
-    const ws = await getBingWallPapers({ n: 10 })
+    if (idx === 0) {
+      const ws = await getBingWallPapers({ idx, n })
+      setWallpapers(ws);
+      setLoading(false)
+      return
+    }
+    if (idx > 0) {
+      if (wallpapers.length > 8) {
+        setLoading(false)
+        return
+      }
+      const ws = await getBingWallPapers({ idx, n })
+      setWallpapers(prev => prev.concat(ws))
+    }
     setLoading(false)
     // console.log(ws)
-    setWallpapers(ws);
+
+
   }
 
   function handleTapItem(wallpaperIndex: number) {
@@ -38,8 +63,12 @@ export default function TabTwoScreen() {
     setModalVisible(true)
   }
 
+  const toggleGalleryLayout = () => {
+    setGalleryLayout(prev => prev === 'list' ? 'grid' : 'list')
+  }
+
   useEffect(() => {
-    handleGetWallpapers(10)
+    handleGetWallpapers(0, 8)
   }, [])
 
   return (
@@ -52,10 +81,10 @@ export default function TabTwoScreen() {
           headerRight: () => (
             loading ? <ActivityIndicator />
               :
-              <Pressable onPress={() => handleGetWallpapers(10)}>
+              <Pressable onPress={toggleGalleryLayout}>
                 {({ pressed }) => (
                   <FontAwesome
-                    name="refresh"
+                    name={galleryLayout === 'list' ? 'th' : "list-ul"}
                     size={l}
                     color={colors.sub}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
@@ -70,37 +99,72 @@ export default function TabTwoScreen() {
 
         <View style={{ flex: 1, height: '100%', width: '100%', padding: s }}>
 
-          <FlashList
-            estimatedItemSize={5}
-            data={wallpapers}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item, index }: { item: Object, index: number }) => {
-              // console.log(item)
-              return (
-                <Pressable
-                  onPress={() => handleTapItem(index)}
-                  style={{
-                    flexDirection: 'row',
-                    flex: 1,
-                    margin: s,
-                    // gap: 8, 
-                    backgroundColor: colors.mid,
-                    borderRadius: m,
-                    overflow: 'hidden'
-                  }}>
-                  <Image source={{ uri: `https://bing.com${item.url}` }}
-                    style={{ height: 100, width: 100 }} />
-                  <View style={{ flex: 1, padding: s }}>
-                    <Text numberOfLines={3} style={text.titleText} >
-                      {item.title}
-                    </Text>
-                    <Text numberOfLines={3} style={text.plainText}>
-                      {item.copyright}
-                    </Text>
-                  </View>
-                </Pressable>
-              )
-            }} />
+          {/* <Text style={text.plainText}>{wallpapers.length}</Text> */}
+          {
+            galleryLayout === 'list' ?
+              <FlashList
+                extraData={colors}
+                // refreshing={loading}
+                // onRefresh={() => handleGetWallpapers(10)}
+                onEndReached={() => {
+                  // console.log('load more.')
+                  handleGetWallpapers(9, 8)
+                }
+                }
+                onEndReachedThreshold={0.1}
+                ListFooterComponentStyle={{ height: 96 }}
+                ListFooterComponent={
+                   <Text style={text.subText}>{wallpapers.length > 8 ? '你已经走到底了' : '...'}</Text>
+                }
+                refreshControl={
+                  <RefreshControl tintColor={colors.sub}
+                    refreshing={loading}
+                    onRefresh={() => handleGetWallpapers(0, 8)} />}
+                estimatedItemSize={5}
+                data={wallpapers}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item, index }: { item: Object, index: number }) => {
+                  // console.log(item)
+                  return (
+                    <Pressable
+                      onPress={() => handleTapItem(index)}
+                      style={{
+                        flexDirection: 'row',
+                        flex: 1,
+                        margin: s,
+                        // gap: 8, 
+                        backgroundColor: colors.mid,
+                        borderRadius: m,
+                        overflow: 'hidden'
+                      }}>
+                      <Image source={{ uri: `https://bing.com${item.url}` }}
+                        style={{ height: 100, width: 100 }} />
+                      <View style={{ flex: 1, padding: s }}>
+                        <Text numberOfLines={3} style={text.titleText} >
+                          {item.title}
+                        </Text>
+                        <Text numberOfLines={3} style={text.plainText}>
+                          {item.copyright}
+                        </Text>
+                      </View>
+                    </Pressable>
+                  )
+                }} />
+              :
+              <ScrollView contentContainerStyle={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
+                {
+                  wallpapers.map((img, index) =>
+                    <Pressable
+                      key={index}
+                      onPress={() => handleTapItem(index)}>
+                      <Image source={{ uri: `https://bing.com${img.url}` }}
+                        style={{ height: 100, width: (screenWidth - s * 2) / 3 }} />
+                    </Pressable>
+                  )
+                }
+              </ScrollView>
+          }
+
         </View>
 
       </View>
